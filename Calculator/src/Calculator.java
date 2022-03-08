57689690e50aec89a7422a2476795defc5f82938
Dog1mo <dpodpo4321@gmail.com>
2022-03-08 19:48:20 +0900

    최초

---
+++ Calculator/src/Calculator.java
@@ -0,0 +1,60 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Calculator extends JFrame {
+		
+	private JTextField inputSpace;
+	
+	
+	public Calculator() {
+		setLayout(null);
+		
+		inputSpace = new JTextField();
+		inputSpace.setEditable(false);
+		inputSpace.setBackground(Color.WHITE);
+		inputSpace.setHorizontalAlignment(JTextField.RIGHT);
+		inputSpace.setFont(new Font("Arial", Font.BOLD, 50));
+		inputSpace.setBounds(8, 10, 270, 70);
+		
+		JPanel buttonpanel = new JPanel();
+		buttonpanel.setLayout(new GridLayout(4, 4, 10, 10));
+		buttonpanel.setBounds(8, 90, 270, 235);
+		
+		String button_names[] = {"C","¡À", "¡¿", "=", "7","8","9","+","4","5","6","-","1","2","3","0"};
+		JButton buttons[]=new JButton [button_names.length];
+		
+		for (int i = 0; i < button_names.length; i++) {
+			buttons[i] = new JButton(button_names[i]);
+		buttons[i].setFont(new Font("Arial", Font.BOLD, 20));
+		if (button_names[i] == "C") buttons[i].setBackground(Color.RED);
+		else if((i>=4 && i<=6) || (i>=8 && i<= 10) || (i>=12 && i<= 14)) buttons[i].setBackground(Color.GRAY); 
+		else buttons[i].setBackground(Color.BLACK);
+		buttons[i].setForeground(Color.WHITE);
+		buttons[i].setBorderPainted(false);
+		buttonpanel.add(buttons[i]);
+		}
+		
+		add(inputSpace);
+		add(buttonpanel);
+		
+		
+		setTitle("°è»ê±â");
+		setVisible(true);
+		setSize(300, 370);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
+	}
+
+	public static void main(String[] args) {
+		new Calculator();
+
+	}
+
+}
